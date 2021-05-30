17a80f108af225600db7adb44d45af1ba9aa4a0f
teshy18 <sebas.tesitore@gmail.com>
2021-05-30 02:33:31 -0300

    cambios varios

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category )=>{
         
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=nFnCMMzAWk0XBazBcMD9X88fuQXizDCx`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=nFnCMMzAWk0XBazBcMD9X88fuQXizDCx`;
 
     const resp = await fetch( url );
     const {data} = await resp.json();
